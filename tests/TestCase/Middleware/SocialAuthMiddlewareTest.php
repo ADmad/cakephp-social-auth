3135556f077bb5c3fcfc8d8ff2c1932ffeaeb06d
ADmad <admad.coder@gmail.com>
2021-10-04 19:27:10 +0530

    Prevent logged in user override.
    If social profile is already associated with a user and it mismatches
    with user record in session (if set) then abort the login instead of
    overwritting the user record in session.
    
    Also the session id is now renewed when user record is written to session.
    
    Closes #108.

--- tests/TestCase/Middleware/SocialAuthMiddlewareTest.php
+++ tests/TestCase/Middleware/SocialAuthMiddlewareTest.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 namespace ADmad\SocialAuth\Test\TestCase\Middleware;
 
 use ADmad\SocialAuth\Middleware\SocialAuthMiddleware;
+use ADmad\SocialAuth\Model\Entity\SocialProfile;
+use ADmad\SocialAuth\Model\Table\SocialProfilesTable;
+use Cake\Event\EventInterface;
+use Cake\Event\EventManager;
 use Cake\Http\Exception\MethodNotAllowedException;
 use Cake\Http\Response;
 use Cake\Http\ServerRequestFactory;
 use Cake\TestSuite\TestCase;
+use SocialConnect\Common\Entity\User;
+use SocialConnect\Provider\AccessTokenInterface;
 use SocialConnect\Provider\Session\Dummy;
 use TestApp\Http\TestRequestHandler;
 
@@ -17,6 +23,11 @@ use TestApp\Http\TestRequestHandler;
  */
 class SocialAuthMiddlewareTest extends TestCase
 {
+    protected $fixtures = [
+        'plugin.ADmad/SocialAuth.Users',
+        'plugin.ADmad/SocialAuth.SocialProfiles',
+    ];
+
     public function setUp(): void
     {
         parent::setUp();
@@ -95,6 +106,61 @@ class SocialAuthMiddlewareTest extends TestCase
         $this->expectException($class);
 
         $middleware = new SocialAuthMiddleware();
+        $middleware->process($request, $this->handler);
+    }
+
+    /**
+     * Test that IDENTITY_MISMATCH_ERROR occurs when a social profile is already
+     * associated with a user and that user does not match the user record set
+     * in the session.
+     *
+     * @return void
+     * @see https://github.com/ADmad/cakephp-social-auth/pull/108
+     */
+    public function testIdentityMismatchError()
+    {
+        $request = ServerRequestFactory::fromGlobals([
+            'REQUEST_URI' => '/social-auth/callback/facebook',
+        ]);
+        $request = $request->withAttribute('params', [
+            'plugin' => 'ADmad/SocialAuth',
+            'controller' => 'Auth',
+            'action' => 'callback',
+            'provider' => 'facebook',
+        ]);
+        $request->getSession()->write('Auth', ['id' => 1]);
+
+        $this->getTableLocator()->get(SocialProfilesTable::class)
+            ->save(new SocialProfile([
+                'user_id' => 2,
+                'provider' => 'facebook',
+                'identifier' => 'fbid',
+                'email' => 'fb@fb.test',
+                'access_token' => '',
+            ]));
+
+        $middleware = $this->getMockBuilder(SocialAuthMiddleware::class)
+            ->onlyMethods(['_getSocialIdentity'])
+            ->getMock();
+
+        $accessToken = $this->getMockBuilder(AccessTokenInterface::class)
+            ->getMock();
+
+        $identity = new User();
+        $identity->id = 'fbid';
+        $identity->email = 'fb@fb.test';
+
+        $middleware->expects($this->once())
+            ->method('_getSocialIdentity')
+            ->willReturn(['identity' => $identity, 'access_token' => $accessToken]);
+
+        EventManager::instance()->on(
+            SocialAuthMiddleware::EVENT_BEFORE_REDIRECT,
+            function (EventInterface $event, $url, string $status) {
+                $this->assertSame(SocialAuthMiddleware::AUTH_STATUS_IDENTITY_MISMATCH, $status);
+            }
+        );
+
         $middleware->process($request, $this->handler);
     }
 }
